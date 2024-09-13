6872f72f51af52e3320d4840a2db0da88f91b17d
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-13 22:05:25 +0000

    Completed 02_06

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,37 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+// #include is a directive to insert the reference file, 
+// <> includes known standard or installed libraries so don't need extenstion like .h or .hpp
+#include <iostream> 
+
+#include <string>
+
+// Provides fixed length definitions for integer types to not depend on primitive types like int or char
+//This stdint one is from C not C++ so thats why it has prefix c
+#include <cstdint> 
+
+//#define defines symbols to mean whatever we type at the right (symbols are called macros), finds and replaces
+// Use all caps for defined constants
+// Every time the preprocessor finds CAPACITY will replace with 5000
+// Do not end with a ;
+#define CAPACITY 5000
+
+// Need for #ifdef DEBUG to work
+// Doesn't need to be assigned if only used for #ifdef directive
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY; //32 bit signed integer from stdint, _t for type
+    uint8_t small = 37; //8 bit unsigned integer
+
+//Include code conditionally with #ifdef (if defined), not usually indented with the code
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+    large += small;
+    std::cout << "The large integer is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
