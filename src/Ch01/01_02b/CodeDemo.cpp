8cb98727eb9e27c8930e90dedad0733f0395bd4b
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-13 20:59:43 +0000

    Completed exercie 01_02

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl; //endl does a new line
+
+    std::cout << std::endl << std::endl; // Used new lines to separate main output from terminal messages
+    return(0); // Traditionally means no errors came up, other numbers for specific errors (error code)
+}
