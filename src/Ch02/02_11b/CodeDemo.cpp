c5bfe597e3e3d8d2bd82aa2c761981b74d748e8c
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-13 23:22:56 +0000

    Completed 02_11

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,24 @@
 #include <cstdint>
 
 int main(){
-    
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5/9.0) * (fahrenheit -32); //5/9 is 0 by int division, convert one to floating point
+
+    std::cout << std::endl;
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "Celsius   : " << celsius << std::endl;
+
+    double weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Float          : " << weight << std::endl;
+    std::cout << "Integer part   : " << (int) weight << std::endl;
+    std::cout << "Fractional part: " << (int)((weight - (int)weight) * 10000) << std::endl;
+    //Floating point numbers are approximations with a small error so get 9899 instead of 9900
+    //Doubles approximate better
+
     std::cout << std::endl << std::endl;
     return (0);
 }
