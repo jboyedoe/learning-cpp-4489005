5bd9fd185f6c7fc42004c6badbb3d3004dab3dfc
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-13 21:14:30 +0000

    Completed 01_03

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,12 +3,20 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> // String header file from standard library
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; //String variable declaration
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> str; // Will hold user input >> as input instead of <<, only works for single words
+    std::cout << "Hello " << str << std::endl;
+
+    // His solution
+    // std::string str; 
+    // std::cout << "Enter your name: " << std::flush; 
+    // !!Flush used to make sure output is completely snet to the terminal before waiting for user input
+    // std::cin >> str;
+    // std::cout << "Nice to meet you, " << str << "!" << std::endl; //Works as concatenation
 
     std::cout << std::endl << std::endl;
     return (0);
