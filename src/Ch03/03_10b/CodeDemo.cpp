b6162a1b2e621518bace5e0240da25690e4dd528
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-16 17:37:20 +0000

    Completed 03_10

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,23 @@
 #include "cow.h"
 
 int main(){
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Betty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Libby", 4, cow_purpose::hide));
+    cattle.push_back(cow("Trudy", 5, cow_purpose::pet));
+    cattle.push_back(cow("Betsy", 2, cow_purpose::dairy));
+
+    //begin returns iterator to the first elem
+    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl; 
+    std::cout << "At index 1 we have " << cattle[1].get_name() << std::endl;
     
+    //Subtract one or use prev because cattle.end returns iterator starting after last element
+    //prev params are the iterator and how many places back to go in it
+    std::cout << "Next to last is " << prev(cattle.end(), 2)->get_name() << std::endl;
+    std::cout << "The last cow is " << (cattle.end() - 1)->get_name() << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
