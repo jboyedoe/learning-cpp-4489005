34ddf8fc1cf74164480b175bc61b67dc3ba89fb2
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-16 18:04:41 +0000

    Completed 04_01

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,30 @@
 #include <iostream>
 
 int main(){
+    int a = 924;
+    bool flag = true;
+    char lttr = 'a';
+
+    if (a > 1000){
+        std::cout << "Warning: a is over 1000." << std::endl;
+    } // Curly bracket not required if only one line
+
+    if (a%2) // Not using ==0 because if a%2 = 0 its if(0) which is if(false) since 0 is false, not too clear
+        std::cout << "a is odd" << std::endl;
+    else
+        std::cout << "a is even" << std::endl;
     
+    std::cout << "The letter " << lttr << " is ";
+    if (lttr != 'a' && lttr != 'e' && lttr != 'i' && lttr != 'o' && lttr != 'u' &&
+        lttr != 'A' && lttr != 'E' && lttr != 'I' && lttr != 'O' && lttr != 'U')
+        std::cout << "not ";
+    std::cout << "a vowel" << std::endl;
+
+    if (flag)
+        std::cout << "The flag is true!" << std::endl;
+    else
+        std::cout << "The flag is false!" << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
