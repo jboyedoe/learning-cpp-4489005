6a22a743b32649d7866833a90c95214bcf81a00b
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-16 17:16:28 +0000

    Completed 03_07

--- src/Ch03/03_07b/CodeDemo.cpp
+++ src/Ch03/03_07b/CodeDemo.cpp
@@ -2,14 +2,22 @@
 // Exercise 03_07
 // Using Objects with Pointers, by Eduardo Corpeño 
 
+// malloc = memory allocation, pointers dynamically allocate memory
+
 #include <iostream>
 #include <string>
 #include "cow.h"
 
 int main(){
-    cow my_cow("Hildy", 7, cow_purpose::pet);
-    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
-    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    cow *my_cow;
+
+    my_cow = new cow("Gertie", 3, cow_purpose::hide); // Now pointer of a cow object
+    //Need to dereference for functions (*mycow) but gets messy so use member of pointer operator (->)
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
+
+    //Delete objects no longer needed to avoid memory leakage when no longer needed, calls destructor
+    delete my_cow; 
     
     std::cout << std::endl << std::endl;
     return (0);
