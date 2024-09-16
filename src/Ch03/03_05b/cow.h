83cf8dd10b77c4a394dfb978c9b076d5c7133520
jboyedoe <72881026+jboyedoe@users.noreply.github.com>
2024-09-16 17:04:57 +0000

    Completed 03_05

---
+++ src/Ch03/03_05b/cow.h
@@ -0,0 +1,23 @@
+#pragma once // Not all compilers support this, use include guard 
+
+//All compilers support include gaurd
+#ifndef COW_H
+#define COW_H
+#include <string>
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+class cow{
+public:
+    cow(std::string name_i, int age_i, cow_purpose purpose_i);
+    std::string get_name() const;
+    int get_age() const;
+    cow_purpose get_purpose() const;
+    void set_age(int new_age);
+private:
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
+#endif // COW_H
